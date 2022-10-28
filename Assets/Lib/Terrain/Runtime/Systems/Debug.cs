8d598057f83c5518e632e1e6fcff81995af89ad9
sjarde@yahoo.fr <0Q5gVpr1HuRUqjts2bFy>
2022-10-28 11:33:10 +0200

    Add player position to debug

--- Assets/Lib/Terrain/Runtime/Systems/Debug.cs
+++ Assets/Lib/Terrain/Runtime/Systems/Debug.cs
@@ -1,3 +1,4 @@
+using FunkySheep.Earth;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
@@ -13,8 +14,30 @@ namespace FunkySheep.Terrain
         }
         public void OnDrawGizmos()
         {
+            Entity player = GetSingletonEntity<SpawnerTag>();
+            LocalToWorldTransform playerPosition;
+            GetComponentLookup<LocalToWorldTransform>().TryGetComponent(player,out playerPosition);
+
             Entities.ForEach((Entity entity, in LocalToWorldTransform localToWorldTransform, in DebugTag debugTag) =>
             {
+                Vector2 player2DPosition = new Vector2(
+                    playerPosition.Value.Position.x,
+                    playerPosition.Value.Position.z
+                );
+
+                Vector2 pointPosition = new Vector2(
+                    localToWorldTransform.Value.Position.x,
+                    localToWorldTransform.Value.Position.z
+                );
+
+
+                if (Vector2.Distance(pointPosition, player2DPosition) < 200)
+                {
+                    Gizmos.color = Color.red;
+                } else
+                {
+                    Gizmos.color = Color.blue;
+                }
                 Gizmos.DrawCube(localToWorldTransform.Value.Position, Vector3.one * 8);
             })
             .WithoutBurst()
